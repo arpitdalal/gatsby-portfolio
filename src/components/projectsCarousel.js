5af9ba982ac190453fc79d94f00fcd9c0ec93a06
Arpit <arpit@reddingdesigns.com>
2021-04-06 19:34:31 -0300

    added some content for home page

---
+++ src/components/projectsCarousel.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Link, useStaticQuery, graphql } from "gatsby"
+import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import Video from '../components/video'
+import 'swiper/swiper-bundle.css'
+import './projectCarousel.css'
+
+SwiperCore.use([Navigation, Pagination, A11y]);
+
+const ProjectsCarousel = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              gif
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={1}
+        allowTouchMove={false}
+        navigation
+        pagination={{
+          type: 'progressbar'
+        }}
+      >
+        {data.allMarkdownRemark.edges.map((edge, index) => {
+          return(
+            <div>
+              <SwiperSlide key={index}>
+                <div className="swiper-scrollbar"></div>
+                <Link className="project-links" to={`/projects/${edge.node.fields.slug}`}>
+                  <Video className="rounded" url={`/${edge.node.frontmatter.gif}`}></Video>
+                  <h4 className="mt-2">{edge.node.frontmatter.title}</h4>
+                </Link>
+              </SwiperSlide>
+            </div>
+          )
+        })}
+      </Swiper>
+    </>
+  )
+}
+
+export default ProjectsCarousel
--- src/components/video.js
+++ src/components/video.js
@@ -5,10 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const video = ({ url }) => {
   return (
     <video
-      className="embed-responsive embed-responsive-16by9"
+      className="embed-responsive embed-responsive-16by9 rounded"
       autoPlay
       muted
       playsInline
+      loop
     >
       <source
         className="embed-responsive-item"
