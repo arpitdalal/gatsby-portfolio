25a7dbd238f0038ffde23e23b69062caaea01077
Arpit <arpit@reddingdesigns.com>
2021-07-20 09:27:17 -0400

    add sitemap and robots.txt plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,65 @@
+const siteUrl = `https://arpitdalal.dev`
+
+Date.prototype.toW3CString = function () {
+  var year = this.getFullYear()
+  var month = this.getMonth()
+  month++
+  if (month < 10) {
+    month = "0" + month
+  }
+  var day = this.getDate()
+  if (day < 10) {
+    day = "0" + day
+  }
+  var hours = this.getHours()
+  if (hours < 10) {
+    hours = "0" + hours
+  }
+  var minutes = this.getMinutes()
+  if (minutes < 10) {
+    minutes = "0" + minutes
+  }
+  var seconds = this.getSeconds()
+  if (seconds < 10) {
+    seconds = "0" + seconds
+  }
+  var offset = -this.getTimezoneOffset()
+  var offsetHours = Math.abs(Math.floor(offset / 60))
+  var offsetMinutes = Math.abs(offset) - offsetHours * 60
+  if (offsetHours < 10) {
+    offsetHours = "0" + offsetHours
+  }
+  if (offsetMinutes < 10) {
+    offsetMinutes = "0" + offsetMinutes
+  }
+  var offsetSign = "+"
+  if (offset < 0) {
+    offsetSign = "-"
+  }
+  return (
+    year +
+    "-" +
+    month +
+    "-" +
+    day +
+    "T" +
+    hours +
+    ":" +
+    minutes +
+    ":" +
+    seconds +
+    offsetSign +
+    offsetHours +
+    ":" +
+    offsetMinutes
+  )
+}
+
+function formattedDate() {
+  const date = new Date();
+  return date.toW3CString();
+}
+
 module.exports = {
   siteMetadata: {
     title: `Arpit Dalal - A full stack web developer`,
@@ -62,5 +124,39 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+        }
+      `,
+        resolveSiteUrl: () => siteUrl,
+        resolvePages: ({ allSitePage: { nodes: allPages } }) => {
+          return allPages.map(page => {
+            return { ...page }
+          })
+        },
+        serialize: ({ path }) => {
+          return {
+            url: path,
+            lastmod: formattedDate(),
+          }
+        },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: siteUrl,
+        sitemap: `${siteUrl}/sitemap/sitemap-index.xml`,
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
   ],
 }
