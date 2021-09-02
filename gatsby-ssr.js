94347e1194bafa098eb849869d1e098664af067f
Arpit Dalal <arpitdalalm@gmail.com>
2021-09-02 10:58:22 -0400

    Better code readability change

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,12 +7,14 @@ exports.onRenderBody = ({ setPreBodyComponents }) => {
         __html: `
           (() => {
             document.addEventListener('DOMContentLoaded', () => {
-              if(!localStorage.getItem('theme'))
-                if(window.matchMedia('(prefers-color-scheme: dark)'))
-                  document.querySelector('body').classList.add('dark-mode')
+              if(!localStorage.getItem('theme')) {
+                if(window.matchMedia('(prefers-color-scheme: dark)')) {
+                  document.querySelector('body').classList.add('dark-mode');
+                }
+              }
               
               if(localStorage.getItem('theme')) {
-                localStorage.getItem('theme') == 'dark-mode' && document.querySelector('body').classList.add('dark-mode')
+                localStorage.getItem('theme') == 'dark-mode' && document.querySelector('body').classList.add('dark-mode');
               }
             })
           })()
