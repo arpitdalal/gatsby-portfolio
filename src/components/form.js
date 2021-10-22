ca6b31a1e97a6ebafb14fc517a0a377897d7271a
arpitdalal <arpitdalal@gmail.com>
2021-10-22 16:14:57 -0400

    add greptcha to the jotform

--- src/components/form.js
+++ src/components/form.js
@@ -11,215 +11,260 @@ const Form = () => {
     q5_phoneNumber: "",
     website: "",
   })
+  const [isHelmet, setIsHelmet] = React.useState(false)
 
   const handleChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
   React.useEffect(() => {
-    ;<Helmet>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/punycode/1.4.1/punycode.min.js"></script>
-      <script
-        src="https://cdn.jotfor.ms/js/vendor/maskedinput.min.js?v=3.3.20399"
-        type="text/javascript"
-      ></script>
-      <script
-        src="https://cdn.jotfor.ms/js/vendor/jquery.maskedinput.min.js?v=3.3.20399"
-        type="text/javascript"
-      ></script>
-      <script
-        src="https://cdn.jotfor.ms/static/prototype.forms.js"
-        type="text/javascript"
-      ></script>
-      <script
-        src="https://cdn.jotfor.ms/static/jotform.forms.js?3.3.20399"
-        type="text/javascript"
-      ></script>
-      <script src="/js/jotform.js" type="text/javascript"></script>
-    </Helmet>
+    setIsHelmet(true)
   }, [])
 
   return (
-    <form
-      className="jotform-form"
-      action="https://submit.jotform.com/submit/202635220764046/"
-      method="post"
-      name="form_202635220764046"
-      id="202635220764046"
-      acceptCharset="utf-8"
-      autoComplete="on"
-    >
-      <input
-        type="hidden"
-        name="formID"
-        onChange={handleChange}
-        value={formData.formID}
-      />
-      <input
-        type="hidden"
-        name="JWTContainer"
-        id="JWTContainer"
-        onChange={handleChange}
-        value={formData.JWTContainer}
-      />
-      <input
-        type="hidden"
-        name="cardinalOrderNumber"
-        id="cardinalOrderNumber"
-        onChange={handleChange}
-        value={formData.cardinalOrderNumber}
-      />
-      <div role="main" className="form-all">
-        <ul className="form-section page-section">
-          <li
-            className="form-line anim jf-required"
-            data-type="control_textbox"
-            id="id_8"
-          >
-            <label
-              className="form-label form-label-left form-label-auto"
-              id="label_8"
-              htmlFor="input_8"
+    <>
+      {isHelmet && (
+        <Helmet>
+          <script
+            src="https://cdn02.jotfor.ms/static/prototype.forms.js"
+            type="text/javascript"
+          ></script>
+          <script
+            src="https://cdn03.jotfor.ms/static/jotform.forms.js?3.3.28643"
+            type="text/javascript"
+          ></script>
+          <script
+            src="https://cdn01.jotfor.ms/js/vendor/jquery-1.8.0.min.js?v=3.3.28643"
+            type="text/javascript"
+          ></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/punycode/1.4.1/punycode.min.js"></script>
+          <script
+            src="https://cdn.jotfor.ms/js/vendor/maskedinput.min.js?v=3.3.20399"
+            type="text/javascript"
+          ></script>
+          <script
+            src="https://cdn.jotfor.ms/js/vendor/jquery.maskedinput.min.js?v=3.3.20399"
+            type="text/javascript"
+          ></script>
+          <script
+            type="text/javascript"
+            src="https://www.google.com/recaptcha/api.js?render=explicit&amp;onload=recaptchaLoadedinput_12"
+          ></script>
+          <script src="/js/jotform.js" type="text/javascript"></script>
+        </Helmet>
+      )}
+      <form
+        className="jotform-form"
+        action="https://submit.jotform.com/submit/202635220764046/"
+        method="post"
+        name="form_202635220764046"
+        id="202635220764046"
+        acceptCharset="utf-8"
+        autoComplete="on"
+      >
+        <input
+          type="hidden"
+          name="formID"
+          onChange={handleChange}
+          value={formData.formID}
+        />
+        <input
+          type="hidden"
+          name="JWTContainer"
+          id="JWTContainer"
+          onChange={handleChange}
+          value={formData.JWTContainer}
+        />
+        <input
+          type="hidden"
+          name="cardinalOrderNumber"
+          id="cardinalOrderNumber"
+          onChange={handleChange}
+          value={formData.cardinalOrderNumber}
+        />
+        <div role="main" className="form-all">
+          <ul className="form-section page-section">
+            <li
+              className="form-line anim jf-required"
+              data-type="control_textbox"
+              id="id_8"
             >
-              Name<span className="form-required">*</span>
-            </label>
-            <div id="cid_8" className="form-input jf-required">
+              <label
+                className="form-label form-label-left form-label-auto"
+                id="label_8"
+                htmlFor="input_8"
+              >
+                Name<span className="form-required">*</span>
+              </label>
+              <div id="cid_8" className="form-input jf-required">
+                <input
+                  type="text"
+                  id="input_8"
+                  name="q8_name"
+                  data-type="input-textbox"
+                  className="form-textbox validate[required]"
+                  size="30"
+                  onChange={handleChange}
+                  value={formData.q8_name}
+                  data-component="textbox"
+                  aria-labelledby="label_8"
+                  required
+                />
+              </div>
+            </li>
+            <li
+              className="form-line anim jf-required"
+              data-type="control_email"
+              id="id_4"
+            >
+              <label
+                className="form-label form-label-left form-label-auto"
+                id="label_4"
+                htmlFor="input_4"
+              >
+                Email<span className="form-required">*</span>
+              </label>
+              <div id="cid_4" className="form-input jf-required">
+                <input
+                  type="email"
+                  id="input_4"
+                  name="q4_email"
+                  className="form-textbox validate[required, Email]"
+                  size="30"
+                  onChange={handleChange}
+                  value={formData.q4_email}
+                  data-component="email"
+                  aria-labelledby="label_4"
+                  required
+                />
+              </div>
+            </li>
+            <li className="form-line anim" data-type="control_phone" id="id_5">
+              <label
+                className="form-label form-label-left form-label-auto"
+                id="label_5"
+                htmlFor="input_5_full"
+              >
+                {" "}
+                Phone Number{" "}
+              </label>
+              <div id="cid_5" className="form-input">
+                <span
+                  className="form-sub-label-container"
+                  style={{ verticalAlign: "top" }}
+                >
+                  <input
+                    type="tel"
+                    id="input_5_full"
+                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    name="q5_phoneNumber"
+                    className="form-textbox validate[Fill Mask]"
+                    autoComplete="off"
+                    onChange={handleChange}
+                    value={formData.q5_phoneNumber}
+                    data-component="phone"
+                    aria-labelledby="label_5"
+                    placeholder="123-456-7890"
+                  />
+                  <label
+                    className="form-sub-label text-muted small"
+                    htmlFor="input_5_full"
+                    id="sublabel_5_masked"
+                    style={{ minHeight: "13px" }}
+                    aria-hidden="false"
+                  >
+                    Format: 123-456-7890
+                  </label>
+                </span>
+              </div>
+            </li>
+            <li
+              className="form-line anim jf-required"
+              data-type="control_textarea"
+              id="id_7"
+            >
+              <label
+                className="form-label form-label-left form-label-auto"
+                id="label_7"
+                htmlFor="input_7"
+              >
+                Message<span className="form-required">*</span>
+              </label>
+              <div id="cid_7" className="form-input jf-required">
+                <textarea
+                  id="input_7"
+                  className="form-textarea validate[required]"
+                  name="q7_typeA"
+                  cols="40"
+                  rows="6"
+                  data-component="textarea"
+                  required
+                  aria-labelledby="label_7"
+                ></textarea>
+              </div>
+            </li>
+            <li
+              className="form-line jf-required"
+              data-type="control_captcha"
+              id="id_12"
+            >
+              <label
+                className="form-label form-label-top"
+                id="label_12"
+                htmlFor="input_12"
+              >
+                Please verify that you are human
+                <span className="form-required">*</span>
+              </label>
+              <div id="cid_12" className="form-input-wide jf-required">
+                <section data-wrapper-react="true">
+                  <div
+                    id="recaptcha_input_12"
+                    data-component="recaptcha"
+                    data-callback="recaptchaCallbackinput_12"
+                    data-expired-callback="recaptchaExpiredCallbackinput_12"
+                  ></div>
+                  <input
+                    type="hidden"
+                    id="input_12"
+                    className="hidden validate[required]"
+                    name="recaptcha_visible"
+                    required=""
+                  />
+                </section>
+              </div>
+            </li>
+            <li className="form-line anim" data-type="control_button" id="id_2">
+              <div id="cid_2" className="form-input-wide">
+                <div
+                  data-align="left"
+                  className="text-left form-buttons-wrapper form-buttons-left jsTest-button-wrapperField"
+                >
+                  <button
+                    id="input_2"
+                    type="submit"
+                    className="ml-0 form-submit-button button submit-button jf-form-buttons jsTest-submitField px-5 py-2"
+                    data-component="button"
+                    data-content=""
+                  >
+                    Send
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="d-none">
+              Should be Empty:
               <input
                 type="text"
-                id="input_8"
-                name="q8_name"
-                data-type="input-textbox"
-                className="form-textbox validate[required]"
-                size="30"
+                name="website"
                 onChange={handleChange}
-                value={formData.q8_name}
-                data-component="textbox"
-                aria-labelledby="label_8"
-                required
+                value={formData.website}
               />
-            </div>
-          </li>
-          <li
-            className="form-line anim jf-required"
-            data-type="control_email"
-            id="id_4"
-          >
-            <label
-              className="form-label form-label-left form-label-auto"
-              id="label_4"
-              htmlFor="input_4"
-            >
-              Email<span className="form-required">*</span>
-            </label>
-            <div id="cid_4" className="form-input jf-required">
-              <input
-                type="email"
-                id="input_4"
-                name="q4_email"
-                className="form-textbox validate[required, Email]"
-                size="30"
-                onChange={handleChange}
-                value={formData.q4_email}
-                data-component="email"
-                aria-labelledby="label_4"
-                required
-              />
-            </div>
-          </li>
-          <li className="form-line anim" data-type="control_phone" id="id_5">
-            <label
-              className="form-label form-label-left form-label-auto"
-              id="label_5"
-              htmlFor="input_5_full"
-            >
-              {" "}
-              Phone Number{" "}
-            </label>
-            <div id="cid_5" className="form-input">
-              <span
-                className="form-sub-label-container"
-                style={{ verticalAlign: "top" }}
-              >
-                <input
-                  type="tel"
-                  id="input_5_full"
-                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                  name="q5_phoneNumber"
-                  className="form-textbox validate[Fill Mask]"
-                  autoComplete="off"
-                  onChange={handleChange}
-                  value={formData.q5_phoneNumber}
-                  data-component="phone"
-                  aria-labelledby="label_5"
-                  placeholder="123-456-7890"
-                />
-                <label
-                  className="form-sub-label text-muted small"
-                  htmlFor="input_5_full"
-                  id="sublabel_5_masked"
-                  style={{ minHeight: "13px" }}
-                  aria-hidden="false"
-                >
-                  Format: 123-456-7890
-                </label>
-              </span>
-            </div>
-          </li>
-          <li
-            className="form-line anim jf-required"
-            data-type="control_textarea"
-            id="id_7"
-          >
-            <label
-              className="form-label form-label-left form-label-auto"
-              id="label_7"
-              htmlFor="input_7"
-            >
-              Message<span className="form-required">*</span>
-            </label>
-            <div id="cid_7" className="form-input jf-required">
-              <textarea
-                id="input_7"
-                className="form-textarea validate[required]"
-                name="q7_typeA"
-                cols="40"
-                rows="6"
-                data-component="textarea"
-                required
-                aria-labelledby="label_7"
-              ></textarea>
-            </div>
-          </li>
-          <li className="form-line anim" data-type="control_button" id="id_2">
-            <div id="cid_2" className="form-input-wide">
-              <div
-                data-align="left"
-                className="text-left form-buttons-wrapper form-buttons-left jsTest-button-wrapperField"
-              >
-                <button
-                  id="input_2"
-                  type="submit"
-                  className="ml-0 form-submit-button button submit-button jf-form-buttons jsTest-submitField px-5 py-2"
-                  data-component="button"
-                  data-content=""
-                >
-                  Send
-                </button>
-              </div>
-            </div>
-          </li>
-          <li className="d-none">
-            Should be Empty:
-            <input
-              type="text"
-              name="website"
-              onChange={handleChange}
-              value={formData.website}
-            />
-          </li>
-        </ul>
-      </div>
-    </form>
+            </li>
+          </ul>
+        </div>
+      </form>
+    </>
   )
 }
 
--- static/js/jotform.js
+++ static/js/jotform.js
@@ -1,21 +1,71 @@
+var recaptchaLoadedinput_12 = function () {
+  window.grecaptcha.render($("recaptcha_input_12"), {
+    sitekey: "6LdU3CgUAAAAAB0nnFM3M3T0sy707slYYU51RroJ",
+  })
+  var grecaptchaBadge = document.querySelector(".grecaptcha-badge")
+  if (grecaptchaBadge) {
+    grecaptchaBadge.style.boxShadow = "gray 0px 0px 2px"
+  }
+}
+
+/**
+ * Called when the reCaptcha verifies the user is human
+ * For invisible reCaptcha;
+ *   Submit event is stopped after validations and recaptcha is executed.
+ *   If a challenge is not displayed, this will be called right after grecaptcha.execute()
+ *   If a challenge is displayed, this will be called when the challenge is solved successfully
+ *   Submit is triggered to actually submit the form since it is stopped before.
+ */
+var recaptchaCallbackinput_12 = function () {
+  var isInvisibleReCaptcha = false
+  var hiddenInput = $("input_12")
+  hiddenInput.setValue(1)
+  if (!isInvisibleReCaptcha) {
+    if (hiddenInput.validateInput) {
+      hiddenInput.validateInput()
+    }
+  } else {
+    triggerSubmit(hiddenInput.form)
+  }
+
+  function triggerSubmit(formElement) {
+    var button = formElement.ownerDocument.createElement("input")
+    button.style.display = "none"
+    button.type = "submit"
+    formElement.appendChild(button).click()
+    formElement.removeChild(button)
+  }
+}
+
+// not really required for invisible recaptcha
+var recaptchaExpiredCallbackinput_12 = function () {
+  var hiddenInput = $("input_12")
+  hiddenInput.writeAttribute("value", false)
+  if (hiddenInput.validateInput) {
+    hiddenInput.validateInput()
+  }
+}
+
+JotForm.newDefaultTheme = false
+JotForm.extendsNewTheme = false
+JotForm.newPaymentUIForNewCreatedForms = false
+JotForm.clearFieldOnHide = "disable"
+
 JotForm.init(function () {
+  /*INIT-START*/
   if (window.JotForm && JotForm.accessible)
     $("input_8").setAttribute("tabindex", 0)
   JotForm.setPhoneMaskingValidator("input_5_full", "(###) ###-####")
   if (window.JotForm && JotForm.accessible)
     $("input_7").setAttribute("tabindex", 0)
-  JotForm.newDefaultTheme = false
-  JotForm.newPaymentUIForNewCreatedForms = true
-  JotForm.newPaymentUI = true
   JotForm.alterTexts(undefined)
-  JotForm.clearFieldOnHide = "disable"
-  JotForm.submitError = "jumpToFirstError"
+  /*INIT-END*/
 })
 
 JotForm.prepareCalculationsOnTheFly([
   null,
   { name: "heading", qid: "1", text: "Form", type: "control_head" },
-  { name: "submit2", qid: "2", text: "Submit", type: "control_button" },
+  { name: "send", qid: "2", text: "Send", type: "control_button" },
   null,
   {
     description: "",
@@ -49,12 +99,22 @@ JotForm.prepareCalculationsOnTheFly([
     text: "Name",
     type: "control_textbox",
   },
+  null,
+  null,
+  null,
+  {
+    description: "",
+    name: "pleaseVerify",
+    qid: "12",
+    text: "Please verify that you are human",
+    type: "control_captcha",
+  },
 ])
 setTimeout(function () {
   JotForm.paymentExtrasOnTheFly([
     null,
     { name: "heading", qid: "1", text: "Form", type: "control_head" },
-    { name: "submit2", qid: "2", text: "Submit", type: "control_button" },
+    { name: "send", qid: "2", text: "Send", type: "control_button" },
     null,
     {
       description: "",
@@ -87,6 +147,16 @@ setTimeout(function () {
       subLabel: "",
       text: "Name",
       type: "control_textbox",
+    },
+    null,
+    null,
+    null,
+    {
+      description: "",
+      name: "pleaseVerify",
+      qid: "12",
+      text: "Please verify that you are human",
+      type: "control_captcha",
     },
   ])
 }, 20)
