51f06fbc25508d6b28f057693296e2c034c0255d
arpitdalal <arpitdalalm@gmail.com>
2022-07-05 14:41:39 -0400

    fix usa-visa function

--- functions/usa-visa.js
+++ functions/usa-visa.js
@@ -1,36 +1,27 @@
 const nodemailer = require("nodemailer")
 
-const transporter = nodemailer.createTransport(
-  `smtps://${process.env.NODEMAILER_USER}:${process.env.NODEMAILER_PASSWORD}@${process.env.NODEMAILER_HOST}`
-)
-
-exports.handler = async function (callback) {
+exports.handler = function (event, context, callback) {
+  const transporter = nodemailer.createTransport(
+    `smtps://${process.env.NODEMAILER_USER}:${process.env.NODEMAILER_PASSWORD}@${process.env.NODEMAILER_HOST}`
+  )
   const messageOptions = {
     from: "arpitdalalm@gmail.com",
     to: "arpitdalalm@gmail.com",
     subject: "US Visa dates available at Toronto office",
     text: "US Visa dates available at Toronto office",
   }
-  try {
-    transporter.sendMail(messageOptions, err => {
-      if (err) {
-        console.log(`Email error: ${err}`)
-        callback(null, {
-          statusCode: 500,
-          body: `Something went wrong. Email Error: ${err}`,
-        })
-      } else {
-        callback(null, {
-          statusCode: 200,
-          body: "Notification sent",
-        })
-      }
-    })
-  } catch (error) {
-    console.log("Error: ", error)
-    callback(null, {
-      statusCode: 500,
-      body: `Something went wrong. Error: ${error}`,
-    })
-  }
+
+  transporter.sendMail(messageOptions, error => {
+    if (error) {
+      callback(null, {
+        statusCode: 500,
+        body: `Something went wrong. Email Error: ${error}`,
+      })
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: "Notification sent",
+      })
+    }
+  })
 }
