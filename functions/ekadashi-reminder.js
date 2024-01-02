94cd55568ab087525172f4b40a44a9b973b3c1fe
Arpit Dalal <arpitdalalm@gmail.com>
2024-01-02 10:14:59 -0500

    Update ekadashis for 2024

--- functions/ekadashi-reminder.js
+++ functions/ekadashi-reminder.js
@@ -3,104 +3,104 @@ const request = require("request")
 
 const EKADASHIS = [
   {
+    title: "Safala Ekadashi",
+    date: "jan 7",
+  },
+  {
     title: "Putrada Ekadashi",
-    date: "jan 2",
+    date: "jan 21",
   },
   {
     title: "Shattila Ekadashi",
-    date: "jan 18",
+    date: "feb 6",
   },
   {
     title: "Jaya Ekadashi",
-    date: "feb 1",
+    date: "feb 20",
   },
   {
     title: "Vijaya Ekadashi",
-    date: "feb 16",
+    date: "mar 7",
   },
   {
     title: "Amalki Ekadashi",
-    date: "mar 3",
+    date: "mar 20",
   },
   {
     title: "Papmochani Ekadashi",
-    date: "mar 18",
+    date: "apr 5",
   },
   {
     title: "Kamda Ekadashi",
-    date: "apr 1",
+    date: "apr 19",
   },
   {
     title: "Varuthini Ekadashi",
-    date: "apr 16",
+    date: "may 4",
   },
   {
     title: "Mohini Ekadashi",
-    date: "may 1",
+    date: "may 18",
   },
   {
     title: "Apara Ekadashi",
-    date: "may 15",
+    date: "jun 3",
   },
   {
     title: "Bhim Ekadashi",
-    date: "may 31",
+    date: "jun 18",
   },
   {
     title: "Yogini Ekadashi",
-    date: "jun 14",
+    date: "jul 2",
   },
   {
     title: "Devshayani Ekadashi-Niyami Ekadashi",
-    date: "jun 29",
+    date: "jul 17",
   },
   {
     title: "Kamika Ekadashi",
-    date: "jul 13",
-  },
-  {
-    title: "Kamala Ekadashi",
-    date: "jul 29",
-  },
-  {
-    title: "Kamala Ekadashi",
-    date: "aug 12",
+    date: "jul 31",
   },
   {
     title: "Pavitra Ekadashi",
-    date: "aug 27",
+    date: "aug 16",
   },
   {
     title: "Aja Ekadashi",
-    date: "sep 10",
+    date: "aug 29",
   },
   {
     title: "Jal Jhilani Ekadashi",
-    date: "sep 25",
+    date: "sep 14",
   },
   {
     title: "Indira Ekadashi",
-    date: "oct 10",
+    date: "sep 28",
   },
   {
     title: "Pashankusha Ekadashi",
-    date: "oct 25",
+    date: "oct 14",
   },
   {
     title: "Rama Ekadashi",
-    date: "nov 9",
+    date: "oct 28",
   },
   {
     title: "Prabodhini Ekadashi",
-    date: "nov 23",
+    date: "nov 12",
   },
   {
     title: "Uttpatti Ekadashi",
-    date: "dec 9",
+    date: "nov 26",
   },
   {
     title: "Mokshada Ekadashi",
-    date: "dec 23",
+    date: "dec 11",
+  },
+  {
+    title: "Safala Ekadashi",
+    date: "dec 26",
   }
 ]
 
